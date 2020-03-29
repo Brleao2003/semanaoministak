131e28a1eec0a9ab18b1d1b2ac2ad25716057bc5
Brleao2003 <brunolopesleao2003@gmail.com>
2020-03-28 21:33:19 -0300

    Front-end do app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,6 @@ import React, { useState } from 'react';
 import Header from './Header';
 
 function App() {
-  const [counter, setCounter] = useState(0);
-
-  function increment() {
-    setCounter(counter + 1);
-  }
-
   return (
   <div>
     <Header>Contador: {counter}</Header>
